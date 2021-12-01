a1de286ba4578eebddd9d8d6e7b888ec4fb1a7cf
Iacopo02 <91663843+Iacopo02@users.noreply.github.com>
2021-12-01 14:20:44 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
-
+#include "lib.h"
+using namespace std;
 int main(){
+  char word;
+  cin>>word;
+  char output = carattere(word);
+  if(output == '9' ){
+    cout << "errore" <<endl;
+  }else{
+    cout << output <<endl;
+  }
   return 0;
 }
